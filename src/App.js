050d3f51dbc74d12353f78770a7ebec09e0a3214
Catarina Gouveia <catarinajrgouveia@gmail.com>
2022-09-11 10:08:04 +0100

    React Weather App created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="Weather-App">
+        <header className="App-header">
+          <a href="/">Lisbon</a>
+          <a href="/">Paris</a>
+          <a href="/">Sydney</a>
+          <a href="/">San Francisco</a>
+        </header>
+      </div>
+      <footer>
+        This App was coded by{" "}
+        <a href="https://github.com/Tauina" target="_blank" rel="noreferrer">
+          Catarina Gouveia
+        </a>{" "}
+        and is{" "}
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/Tauina/weather-react"
           target="_blank"
-          rel="noopener noreferrer"
+          rel="noreferrer"
         >
-          Learn React
+          open-sourced on GitHub
+        </a>{" "}
+        and{" "}
+        <a
+          href="https://peppy-mermaid-b65ea5.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          hosted on Netlify
         </a>
-      </header>
+        .
+      </footer>
     </div>
   );
 }
