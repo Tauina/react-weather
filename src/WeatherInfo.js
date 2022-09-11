c2ce371e9f7e8638b079b4a0857860e6f03bb8e9
Catarina Gouveia <catarinajrgouveia@gmail.com>
2022-09-11 10:10:44 +0100

    Weather Info created

---
+++ src/WeatherInfo.js
@@ -0,0 +1,34 @@
+import React from "react";
+import WeatherIcon from "./WeatherIcon";
+import FormattedDate from "./FormattedDate";
+import "./WeatherInfo.css";
+
+export default function WeatherInfo(props) {
+  return (
+    <div>
+      <div className="row">
+        <div className="today-info">
+          <h1>{props.data.city}</h1>
+          <FormattedDate date={props.data.date} />
+          <div className="text-capitalize">{props.data.description}</div>
+        </div>
+      </div>
+
+      <div className="row mt-3">
+        <div className="col-sm-6">
+          <div className="icon">
+            <WeatherIcon code={props.data.icon} size="65" />
+          </div>
+          <div className="temp-today">{Math.round(props.data.temperature)}</div>
+          <div className="unit">ºC</div>
+        </div>
+        <div className="col-sm-6">
+          <div className="more-info">
+            <div>Humidity: {props.data.humidity}%</div>
+            <div>Wind: {Math.round(props.data.wind)}km/h</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
