f285a751163c161533e695cc8aa1cd1e81666a90
Catarina Gouveia <catarinajrgouveia@gmail.com>
2022-09-12 13:52:06 +0100

    Forecast day0 added

--- src/Forecast.js
+++ src/Forecast.js
@@ -1,32 +1,34 @@
 import "./Forecast.css";
-import React from "react";
-import WeatherIcon from "./WeatherIcon";
+import React, { useState } from "react";
 import axios from "axios";
+import ForecastDay from "./Forecastday";
 
 export default function Forecast(props) {
+  let [loaded, setLoaded] = useState(false);
+  let [forecast, setForecast] = useState(null);
+
   function handleResponse(response) {
-    console.log(response.data);
+    setForecast(response.data.daily);
+    setLoaded(true);
   }
 
-  let units = "metric";
-  let latitude = props.coordinates.lat;
-  let longitude = props.coordinates.lon;
-  let apikey = `198980771e81f379fdd6a032c49dd66d`;
-  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apikey}&units=${units}`;
-  axios.get(apiUrl).then(handleResponse);
-
-  return (
-    <div className="Forecast">
-      <div className="row">
-        <div className="col-sm">
-          <div className="forecast-day">Wed</div>
-          <WeatherIcon code="01d" size="35" />
-          <div className="forecast-temp">
-            <span className="forecast-temp-max">24º</span>
-            <span className="forecast-temp-min">20º</span>
+  if (loaded) {
+    return (
+      <div className="Forecast">
+        <div className="row">
+          <div className="col-sm">
+            <ForecastDay data={forecast[0]}/>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    let units = "metric";
+    let latitude = props.coordinates.lat;
+    let longitude = props.coordinates.lon;
+    let apikey = `198980771e81f379fdd6a032c49dd66d`;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apikey}&units=${units}`;
+    axios.get(apiUrl).then(handleResponse);
+    return null;
+  }
 }
