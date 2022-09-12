ab7e5607ad01252b636b2ec17b3ef565eb655bc9
Catarina Gouveia <catarinajrgouveia@gmail.com>
2022-09-12 17:54:30 +0100

    bootstrap Forecast edited

--- src/Forecast.js
+++ src/Forecast.js
@@ -19,7 +19,7 @@ export default function Forecast(props) {
           {forecast.map(function (dailyForecast, index) {
             if (index < 5) {
               return (
-                <div className="col-sm" key={index}>
+                <div className="col" key={index}>
                   <ForecastDay data={dailyForecast} />
                 </div>
               );
