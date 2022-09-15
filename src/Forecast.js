b633fd47d6e524fd4ab4b00578005f92f88689aa
Catarina Gouveia <catarinajrgouveia@gmail.com>
2022-09-15 19:13:38 +0100

    Forecast refactored

--- src/Forecast.js
+++ src/Forecast.js
@@ -11,6 +11,17 @@ export default function Forecast(props) {
     setForecast(response.data.daily);
     setLoaded(true);
   }
+
+  function load() {
+    let units = "metric";
+    let latitude = props.coordinates.lat;
+    let longitude = props.coordinates.lon;
+    let apikey = `198980771e81f379fdd6a032c49dd66d`;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apikey}&units=${units}`;
+    axios.get(apiUrl).then(handleResponse);
+    return null;
+  }
+
   useEffect(() => {
     setLoaded(false);
   }, [props.coordinates]);
@@ -33,12 +44,6 @@ export default function Forecast(props) {
       </div>
     );
   } else {
-    let units = "metric";
-    let latitude = props.coordinates.lat;
-    let longitude = props.coordinates.lon;
-    let apikey = `198980771e81f379fdd6a032c49dd66d`;
-    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apikey}&units=${units}`;
-    axios.get(apiUrl).then(handleResponse);
-    return null;
+    load();
   }
 }
